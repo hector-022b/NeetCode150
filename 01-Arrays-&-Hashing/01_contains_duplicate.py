aaf7fc305cf641a10d1dd438987958b2c2516afd
Hector Barahona <127160503+hector-022b@users.noreply.github.com>
2025-06-10 15:32:54 -0700

    Update 01_contains_duplicate.py

--- 01-Arrays-&-Hashing/01_contains_duplicate.py
+++ 01-Arrays-&-Hashing/01_contains_duplicate.py
@@ -1,1 +1,10 @@
 
+
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) != len(set(nums)):
+            return True
+        else: 
+            return False
